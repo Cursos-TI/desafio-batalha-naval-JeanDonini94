9591aa728cbe366641aabbbead2e8d3f6446ce64
JeanDonini94 <107122365+JeanDonini94@users.noreply.github.com>
2025-04-07 19:12:33 +0000

    ATUALIZAÇÃO BATALHA NAVAL

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,93 @@
 #include <stdio.h>
+#include <stdlib.h> 
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define SIZE 10
+#define HSIZE 5
+
+void Tabuleiro(int board[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            printf("%d ", board[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void habilidade(int board[SIZE][SIZE], int skill[HSIZE][HSIZE], int originX, int originY) {
+    int startX = originX - HSIZE / 2;
+    int startY = originY - HSIZE / 2;
+
+    for (int i = 0; i < HSIZE; i++) {
+        for (int j = 0; j < HSIZE; j++) {
+            int x = startX + i;
+            int y = startY + j;
+            if (x >= 0 && x < SIZE && y >= 0 && y < SIZE && skill[i][j] == 1) {
+                board[x][y] = 5;
+            }
+        }
+    }
+}
+//Criando as habilidades
+void Cone(int skill[HSIZE][HSIZE]) {
+    for (int i = 0; i < HSIZE; i++) {
+        for (int j = 0; j < HSIZE; j++) {
+            if (j >= HSIZE / 2 - i && j <= HSIZE / 2 + i) {
+                skill[i][j] = 1;
+            } else {
+                skill[i][j] = 0;
+            }
+        }
+    }
+}
+
+void Cruz(int skill[HSIZE][HSIZE]) {
+    for (int i = 0; i < HSIZE; i++) {
+        for (int j = 0; j < HSIZE; j++) {
+            if (i == HSIZE / 2 || j == HSIZE / 2) {
+                skill[i][j] = 1;
+            } else {
+                skill[i][j] = 0;
+            }
+        }
+    }
+}
+
+void Octaedro(int skill[HSIZE][HSIZE]) {
+    for (int i = 0; i < HSIZE; i++) {
+        for (int j = 0; j < HSIZE; j++) {
+            if (abs(i - HSIZE / 2) + abs(j - HSIZE / 2) <= HSIZE / 2) {
+                skill[i][j] = 1;
+            } else {
+                skill[i][j] = 0;
+            }
+        }
+    }
+}
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int board[SIZE][SIZE] = {0};
+    int coneSkill[HSIZE][HSIZE];
+    int cruzSkill[HSIZE][HSIZE];
+    int octaedroSkill[HSIZE][HSIZE];
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    // Definindo um navio no tabuleiro
+    board[4][4] = 3;
+    board[4][5] = 3;
+    board[4][6] = 3;
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    // Criando habilidades
+    Cone(coneSkill);
+    Cruz(cruzSkill);
+    Octaedro(octaedroSkill);
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    // Aplicando habilidades no tabuleiro
+    habilidade(board, coneSkill, 2, 2);
+    habilidade(board, cruzSkill, 5, 5);
+    habilidade(board, octaedroSkill, 7, 7);
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    // Exibindo o tabuleiro
+    Tabuleiro(board);
 
     return 0;
 }
